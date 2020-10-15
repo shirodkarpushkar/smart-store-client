1858c880d11c01308caf9aeaed59636f66795f07
shirodkarpushkar <pushkars@winjit.com>
2020-10-15 16:16:36 +0530

    loading page

--- src/router/views/_loading.vue
+++ src/router/views/_loading.vue
@@ -1,39 +1,20 @@
+<template>
+  <div class="loader">
+    <a-spin />
+  </div>
+</template>
+
 <script>
-import Layout from '@layouts/main'
+
 
 export default {
   page: {
     title: 'Loading page...',
     meta: [{ name: 'description', content: 'Loading page...' }],
   },
-  components: { Layout },
+  components: {  },
 }
 </script>
 
-<template>
-  <Layout>
-    <Transition appear>
-      <!-- <BaseIcon :class="$style.loadingIcon" name="sync" spin /> -->
-    </Transition>
-  </Layout>
-</template>
 
-<style lang="scss" module>
-@import '@design';
-
-.loadingIcon {
-  @extend %typography-xxlarge;
-
-  display: block;
-  margin: 0 auto;
-
-  // stylelint-disable-next-line selector-class-pattern
-  &:global(.v-enter-active) {
-    transition: opacity 1s;
-  }
-  // stylelint-disable-next-line selector-class-pattern
-  &:global(.v-enter) {
-    opacity: 0;
-  }
-}
-</style>
+<style scoped></style>
