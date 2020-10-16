fc5326990bd74413f8c94861fb2578067bc5c552
shirodkarpushkar <pushkars@winjit.com>
2020-10-16 18:47:28 +0530

    sidebar, landing page and navbar

--- src/router/layouts/userLayout.vue
+++ src/router/layouts/userLayout.vue
@@ -3,7 +3,7 @@
     <a-layout-header class="nav-head">
       <div class="brand">
         <div class="brand-drawer">
-          <a-icon type="menu-fold" @click="openDrawer" />
+          <a-icon type="menu" @click="openDrawer" />
         </div>
 
         <div class="brand-img">
@@ -32,7 +32,9 @@
               >
               <a-menu-item><a-icon type="read" />About Us</a-menu-item>
               <a-menu-item><a-icon type="phone" />Contact Us</a-menu-item>
-              <a-menu-item @click="signOutUser"><a-icon type="logout" />Sign Out</a-menu-item>
+              <a-menu-item @click="signOutUser"
+                ><a-icon type="logout" />Sign Out</a-menu-item
+              >
             </a-menu>
           </a-dropdown>
         </a-menu-item>
@@ -45,15 +47,63 @@
     </a-layout-content>
     <layout-footer></layout-footer>
     <a-drawer
-      title="Basic Drawer"
+      title="Smart Store v1.0"
       placement="left"
       :closable="false"
       :visible="showDrawer"
       @close="onClose"
-    >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      ><a-avatar style="" :size="64" :src="imgBaseUrl + user.result.avatar" />
+      <h3 class="mt-1"
+        >Hello, {{ user.result.firstName }} {{ user.result.lastName }}</h3
+      >
+      <p>{{ user.result.email }}</p>
+      <a-divider type="horizontal" />
+
+      <a-menu
+        mode="inline"
+        :default-open-keys="['category', 'account']"
+        class="drawer-menu"
+      >
+        <a-sub-menu key="category">
+          <span slot="title"
+            ><a-icon type="apartment" /><span> Avalaible Categories</span></span
+          >
+          <a-menu-item>
+            Home and Garden
+          </a-menu-item>
+          <a-menu-item>
+            Electronic
+          </a-menu-item>
+          <a-menu-item>
+            Fashion
+          </a-menu-item>
+          <a-menu-item>
+            Music
+          </a-menu-item>
+          <a-menu-item>
+            Sports
+          </a-menu-item>
+          <a-menu-item>
+            Gaming
+          </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="account">
+          <span slot="title"><a-icon type="user" /><span>Personal</span></span>
+          <a-menu-item>
+            Your Account
+          </a-menu-item>
+          <a-menu-item>
+            Your Orders
+          </a-menu-item>
+          <a-menu-item>
+            Favourites
+          </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+      <a-divider type="horizontal" />
+      <h4 class="mt-1">Contact Us</h4>
+      <h4 class="mt-1">About Us</h4>
+      <h4 class="mt-1">Sign Out</h4>
     </a-drawer>
   </a-layout>
 </template>
@@ -114,5 +164,13 @@ export default {
 }
 .brand-drawer {
   margin: 0 0 0 1rem;
+}
+.drawer-menu {
+  border: none;
+  margin-left: -1.5rem;
+}
+.ant-menu-item .anticon + span, .ant-menu-submenu-title .anticon + span {
+  font-weight: 500;
+
 }
 </style>
--- src/router/views/customer/home.vue
+++ src/router/views/customer/home.vue
@@ -1,6 +1,56 @@
 <template>
   <div>
-    Home Page
+    <a-carousel :autoplay="false" arrows>
+      <div
+        slot="prevArrow"
+        class="custom-slick-arrow"
+        style="left: 10px;zIndex: 1"
+      >
+        <a-icon type="left-circle" />
+      </div>
+
+      <div slot="nextArrow" class="custom-slick-arrow" style="right: 10px">
+        <a-icon type="right-circle" />
+      </div>
+
+      <div
+        class="img-background"
+        :style="{
+          backgroundImage: 'url(' + require('@assets/images/d1.jpg') + ')',
+        }"
+      >
+        <h1 align="left"
+          >Get Up to 40% off on Headsets and other electronic items.</h1
+        >
+      </div>
+
+      <div
+        class="img-background"
+        :style="{
+          backgroundImage: 'url(' + require('@assets/images/d2.jpg') + ')',
+        }"
+      >
+        <h1 align="left">Fashion Items , Make up Items Get Up to 50% off .</h1>
+      </div>
+
+      <div
+        class="img-background"
+        :style="{
+          backgroundImage: 'url(' + require('@assets/images/d3.jpg') + ')',
+        }"
+      >
+       <h1 align="left">Mens ,Womens and children watches to 10% off, Hurry!!</h1>
+      </div>
+
+      <div
+        class="img-background"
+        :style="{
+          backgroundImage: 'url(' + require('@assets/images/d4.png') + ')',
+        }"
+      >
+        <h1 align="left">Electronics Items to 60% off, Deal closes on 10 December 2020 Hurry!!</h1>
+      </div>
+    </a-carousel>
   </div>
 </template>
 
@@ -18,3 +68,43 @@ export default {
   },
 }
 </script>
+<style scoped>
+/* For demo */
+.ant-carousel >>> .slick-slide {
+  text-align: center;
+  height: 80vh;
+  line-height: 160px;
+  background: #364d79;
+}
+
+.ant-carousel .custom-slick-arrow {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: #fff;
+  background-color: rgba(31, 45, 61, 0.11);
+  opacity: 0.3;
+}
+.ant-carousel .custom-slick-arrow:before {
+  display: none;
+}
+.ant-carousel .custom-slick-arrow:hover {
+  opacity: 0.5;
+}
+
+.ant-carousel .slick-slide h1 {
+  color: #fff;
+  font-size: 2rem;
+  margin: 16rem 4rem 0;
+}
+.img-background {
+  width: 100%;
+  height: 80vh;
+  display: inline-block;
+  background-image: url(/img/d1.f938d227.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position-x: center;
+  background-position-y: center;
+}
+</style>
